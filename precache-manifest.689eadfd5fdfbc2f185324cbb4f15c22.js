4b75010123ad121e539cad3f5262ca308327e74d
vbach <vlminder@student.fullsail.edu>
2019-03-27 00:58:01 -0400

    Updates

---
+++ precache-manifest.689eadfd5fdfbc2f185324cbb4f15c22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "063273b0e9fe9ddfee2c",
+    "url": "/movieapp/static/js/runtime~main.03f68af8.js"
+  },
+  {
+    "revision": "29d26f06b906cdb35ddc",
+    "url": "/movieapp/static/js/main.001b4c1d.chunk.js"
+  },
+  {
+    "revision": "46cd34a5ff079f5bd446",
+    "url": "/movieapp/static/js/2.1aeaa885.chunk.js"
+  },
+  {
+    "revision": "29d26f06b906cdb35ddc",
+    "url": "/movieapp/static/css/main.53892cb9.chunk.css"
+  },
+  {
+    "revision": "ab3bb8f7bead63a2225b4c6c34997979",
+    "url": "/movieapp/index.html"
+  }
+];
